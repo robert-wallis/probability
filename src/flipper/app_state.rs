e53b13b3929d7b5e78f4f134a1c79b31ab1cf66e
Robert Wallis <smilingrob@gmail.com>
2023-05-12 21:51:55 -0700

    100% coverage on app_state

--- src/flipper/app_state.rs
+++ src/flipper/app_state.rs
@@ -1,19 +1,29 @@
 use std::collections::BTreeMap;
 use std::fmt;
 
-// information about the current random generation
+/// information about the current random generation
 pub struct AppState {
-    pub last_result: bool,               // last roll
-    pub current_result: bool,            // current roll
-    pub current_run: u32,                // current streak of the same result
-    pub longest_run: u32,                // largest streak of the same result
-    pub run_lengths: BTreeMap<u32, u32>, // <length, times_occurred>
-    pub count_result_in_a_run: u32,      // how many results were in runs
-    pub total_count: u32,                // total number of rolls
-    pub current_id: u32,                 // the current run index x of `total_count`
+    /// last roll
+    pub last_result: bool,
+    /// current roll
+    pub current_result: bool,
+    /// current streak of the same result
+    pub current_run: u32,
+    /// largest streak of the same result
+    pub longest_run: u32,
+    /// <length, times_occurred>
+    pub run_lengths: BTreeMap<u32, u32>,
+    /// how many results were in runs
+    pub count_result_in_a_run: u32,
+    /// total number of rolls
+    pub total_count: u32,
+    /// the current run index x of `total_count`
+    pub current_id: u32,
 }
 
 impl AppState {
+    /// create a new state
+    /// total_count: the intended number of state changes
     pub fn new(total_count: u32) -> AppState {
         AppState {
             last_result: false,
@@ -27,6 +37,7 @@ impl AppState {
         }
     }
 
+    /// moves self into a new state, calculating the internal sequence length values
     pub fn next(mut self, result: bool) -> AppState {
         let last_result = self.current_result;
         let is_run = last_result == result;
@@ -51,6 +62,10 @@ impl AppState {
     }
 }
 
+/// formats the AppState to return for example:
+/// > total rolls: 10
+/// > total runs: 7, (0.7%)
+/// > longest run: 4
 impl fmt::Display for AppState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "total rolls: {}", self.total_count)?;
@@ -146,11 +161,77 @@ mod tests {
     }
 
     #[test]
+    fn next_saved_context() {
+        // GIVEN a list of boolean values
+        let list = [
+            true, false, false, false, true, false, true, true, true, true, true, false, false,
+            false, false, false, false, false, false, true, true, false, true, false,
+        ];
+
+        let mut state = AppState::new(list.len() as u32);
+
+        // WHEN the next() function is called on that list
+        for b in list {
+            state = state.next(b);
+        }
+
+        // THEN the expected result should be whatever it was when I programmed it
+
+        // programmatically determining these values
+        let max_sequence = largest_sequence(&list);
+        let list_last = *list.last().expect("bad test");
+        assert_eq!(list_last, state.current_result, "current_result");
+        assert_eq!(list.len() as u32, state.total_count, "total_count");
+        assert_eq!(max_sequence as u32, state.longest_run, "longest_run");
+        assert_eq!(list.len() as u32, state.current_id, "current_id");
+
+        // eyeballing these values
+        assert_eq!(1, state.current_run, "current_run");
+        assert_eq!(14, state.count_result_in_a_run, "count_result_in_a_run");
+    }
+
+    #[test]
     fn display() {
         let a = AppState::new(1);
         assert_eq!(
             "total rolls: 1\ntotal runs: 0 (0%)\nlongest run: 0\n",
             a.to_string()
+        );
+    }
+
+    /// Of the three correct implementations, the one that has the best readability is the following `largest_sequence`
+    /// This implementation is straightforward and easy to understand. It uses a simple loop and conditional statements to keep track of the current and maximum sequences. The variable names (`max_sequence`, `current_sequence`, `previous_item`) are descriptive and help to clarify their purpose.
+    /// The functional versions of the code, while concise, might be less readable for developers who are not familiar with functional programming concepts. They involve higher-order functions, closures, and methods like `fold`, `map`, and `unwrap_or`, which can introduce additional complexity for those who are new to these concepts.
+    /// Therefore, in terms of readability, the initial imperative implementation with explicit loop and conditionals tends to be more approachable and easier to understand for most developers.
+    /// -- Coding AI, 2023
+    fn largest_sequence(list: &[bool]) -> usize {
+        let mut max_sequence = 0;
+        let mut current_sequence = 0;
+        let mut previous_item: Option<bool> = None;
+
+        for &item in list {
+            if let Some(prev_item) = previous_item {
+                if item != prev_item {
+                    current_sequence = 0;
+                }
+                current_sequence += 1;
+            }
+            max_sequence = max_sequence.max(current_sequence);
+            previous_item = Some(item);
+        }
+
+        max_sequence
+    }
+
+    #[test]
+    fn test_largest_sequence() {
+        assert_eq!(
+            3,
+            largest_sequence(&[true, false, false, false, true, true])
+        );
+        assert_eq!(
+            4,
+            largest_sequence(&[false, true, true, true, true, false, false, false, true, false])
         );
     }
 }
