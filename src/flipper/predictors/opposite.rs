a5526daa16dcc49f11780fbbc1a561481d6e4efd
Robert Wallis <smilingrob@gmail.com>
2023-05-14 21:57:35 -0700

    opposite 100% coverage

--- src/flipper/predictors/opposite.rs
+++ src/flipper/predictors/opposite.rs
@@ -36,3 +36,57 @@ impl fmt::Display for Prediction {
         write!(f, "Opposite")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{AppState, Better, Prediction, Predictor};
+
+    #[test]
+    fn chooses_the_opposite() {
+        // GIVEN the flipper predictor
+        let mut p = Prediction::new();
+        let mut app_state = AppState::new(0);
+
+        // WHEN predict is called
+        app_state = app_state.next(true);
+        let first = p.predict(&app_state);
+
+        app_state = app_state.next(false);
+        let second = p.predict(&app_state);
+
+        // THEN it should predict the opposite
+        assert!(!first, "first should be false");
+        assert!(second, "second should be true");
+    }
+
+    #[test]
+    fn bets_double_down_every_other_time() {
+        // GIVEN the opposite predictor
+        let mut p = Prediction::new();
+        let mut app_state = AppState::new(0);
+
+        // WHEN bet is called
+        let first = p.bet(&app_state).unwrap();
+        app_state = app_state.next(true);
+        let second = p.bet(&app_state).unwrap();
+        app_state = app_state.next(false);
+        let third = p.bet(&app_state).unwrap();
+        app_state = app_state.next(true);
+        let fourth = p.bet(&app_state).unwrap();
+
+        // THEN it should bet double down every other time
+        assert_eq!(first.wager, 2, "first bet should be double down");
+        assert_eq!(second.wager, 2, "second bet should be double down");
+        assert_eq!(third.wager, 1, "third bet should be single down");
+        assert_eq!(fourth.wager, 1, "fourth bet should be single down");
+    }
+
+    #[test]
+    fn name() {
+        assert_eq!(
+            "Opposite".to_string(),
+            Prediction::new().to_string(),
+            "name is Opposite"
+        );
+    }
+}
